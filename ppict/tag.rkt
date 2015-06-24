01ced10649b28dee3d04b4793569b3f3719674d7
Ryan Culpepper <ryanc@racket-lang.org>
2015-06-24 17:33:05 -0400

    prepare for extraction of ppict code
    original commit: dedff723f7841d4a31582a94297d3d6313b8e6f5

---
+++ ppict/tag.rkt
@@ -0,0 +1,16 @@
+#lang racket/base
+(require pict
+         "private/tag-pict.rkt")
+
+(provide/contract
+ [tag-path?
+  (-> any/c boolean?)]
+ [tag-pict
+  (-> pict? symbol? pict?)]
+ [pict-tag
+  (-> pict? (or/c symbol? #f))]
+ [find-tag
+  (-> pict? tag-path? (or/c pict-path? #f))]
+ [find-tag*
+  (-> pict? tag-path?
+      (listof pict-path?))])
